20624fdd566329a474c9ca829718dd62d93206fc
Sean Clemmer <sczizzo@gmail.com>
2011-07-11 17:29:06 -0500

    Removed duplicate Devise views, added easier login-family routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ Dhaka::Application.routes.draw do
   %w( terms privacy safety issues about faqs status ).each do |page|
     match page => 'high_voltage/pages#show', :id => page
   end
+
+  devise_scope :user do
+    get "register", :to => "devise/registrations#new"
+    get "login",    :to => "devise/sessions#new"
+    get "logout",   :to => "devise/sessions#destroy"
+  end
 end
