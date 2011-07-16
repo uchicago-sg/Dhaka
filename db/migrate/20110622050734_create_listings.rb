1aca832ac777eaf411923bc9508dce19874f5f67
Sean Clemmer <sczizzo@gmail.com>
2011-07-15 23:11:16 -0500

    Moved footer to sidebar tab, update to Listing prices

--- db/migrate/20110622050734_create_listings.rb
+++ db/migrate/20110622050734_create_listings.rb
@@ -3,7 +3,7 @@ class CreateListings < ActiveRecord::Migration
     create_table :listings do |t|
       t.string :description
       t.text :details
-      t.integer :price
+      t.decimal :price, :precision => 8, :scale => 2, :default => 0
       t.integer :status
       t.references :seller
 
