c655e48e5e35f8aa5a0070f68b002701c8e67e75
Paul Kaplan <paul.kaplan.74@gmail.com>
2011-09-01 19:27:53 -0700

    Changed session to array, changed html

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -1,16 +1,15 @@
 class ComparisonsController < ApplicationController
 
 def create
-  # Could use +=, but the handy properties of nil.to_s get around the initial setting issue, whereas += does not.
-  # They should change += to += unless nil else =
-  session[:compare] = session[:compare].to_s + "&#{params[:id]}"
+  session[:compare] ||= [] 
+  session[:compare] << params[:id].to_i
   render :nothing => true
 end
 
 def index
   @listings = []
   if session[:compare]
-    session[:compare].split("&").each do |id|
+    session[:compare].each do |id|
       @listings << Listing.find(id) unless id.blank?
     end
   end
