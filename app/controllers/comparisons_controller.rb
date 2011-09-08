848153f184c2829552b50479646ec124987fe25b
Sean Clemmer <sczizzo@gmail.com>
2011-09-07 22:06:19 -0500

    Fixing star/unstar mechanism, adding auto_discovery_link_tags

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -2,14 +2,14 @@ class ComparisonsController < ApplicationController
   # POST /compare
   def create
     session[:starred] ||= []
-    session[:starred] << params['id'].to_i
+    session[:starred] << params[:permalink]
     flash[:notice] = 'Successfully starred listing.'
     redirect_to :back
   end
 
-  # GET /compare/:id
+  # GET /compare/:permalink
   def update
-    if session[:starred].delete params['id'].to_i
+    if session[:starred].delete params[:permalink]
       flash[:notice] = 'Successfully unstarred listing.'
     else
       flash[:notice] = 'No such listing to unstar.'
@@ -22,13 +22,12 @@ class ComparisonsController < ApplicationController
     @listings = []
     if session[:starred]
       session[:starred].uniq!
-      session[:starred].each do |listing_id|
-        @listings << Listing.find(listing_id) rescue nil
+      session[:starred].each do |listing_permalink|
+        @listings << Listing.find_by_permalink(listing_permalink) rescue nil
       end
     end
     @listings.compact!
-    @listings.keep_if { |l| can? :read, l }
-    session[:starred] = @listings.map &:id
+    session[:starred] = @listings.map &:permalink
     @listings.reverse!
   end
 end
