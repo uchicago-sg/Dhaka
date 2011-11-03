3c97e1c914edb2304a5681ce2d8c3b11218c3cd4
Paul Kaplan <paul.kaplan.74@gmail.com>
2011-11-02 22:46:45 -0500

    Explicitly declare paperclip path and url to fix urls with spaces

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,9 @@
 class Image < ActiveRecord::Base
   belongs_to :listing
-  has_attached_file :photo, :styles => {
+  has_attached_file :photo, 
+  :url  => "/assets/images/:id/:style/:basename.:extension",
+  :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension",
+  :styles => {
     :large => "400x400>", # If either width or height > 400px, it will be scaled down proportional to original
     :small => "200x200#",
     :thumb => "100x100#"
