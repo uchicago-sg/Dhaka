cf258f6f38cd867cc4159a426ac1d33ebbae5985
Paul Kaplan <paul.kaplan.74@gmail.com>
2011-11-06 19:50:17 -0600

    Temporary fix for nbsp stripping in rows

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,4 +1,8 @@
 module ListingsHelper
+  def strip_tags_and_spaces(string)
+    strip_tags string.gsub!("&nbsp;", " ")
+  end
+  
   def show_more
     link_to "More", "", :class => "show-more", :id => "expand"
   end
