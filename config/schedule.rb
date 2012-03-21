d0e44509756bbdc72dfefc42afd7b24f9ee152f3
Sean Clemmer <sczizzo@gmail.com>
2012-03-21 01:20:32 -0500

    Scheduling whenever to run at 1:00 am instead of midnight

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,4 @@
-# set :environment => 'development'
-# set :output, "path/to/cron/log"
-
-every 1.day do
+every 1.day, :at => '1:00 am' do
   runner "Listing.remove_expired_images"
   runner "Listing.notify_almost_renewable"
 end
