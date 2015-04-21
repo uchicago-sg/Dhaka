5d7aec4f2f1176b2fee4b1de2d4100fe682c3f5c
Jeremy Archer <open-source@fatlotus.com>
2015-04-21 12:22:19 -0500

    Reformat image views in listing.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,12 +29,12 @@ Dhaka::Application.configure do
   config.assets.compress = false
   
   # Add image uploads to S3.
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentails => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  # config.paperclip_defaults = {
+  #   :storage => :s3,
+  #   :s3_credentails => {
+  #     :bucket => ENV['S3_BUCKET_NAME'],
+  #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #   }
+  # }
 end
